8758c0b4ba988816badcc51d2b43767c53b177eb
Lee Grey <leegrey.nz@gmail.com>
2025-11-18 12:34:10 +1300

    update manifest

--- source/com.mothteeth.WaveformTuner.xrnx/main.lua
+++ source/com.mothteeth.WaveformTuner.xrnx/main.lua
@@ -1,14 +1,14 @@
 require "WaveformTuner"
 
 renoise.tool():add_menu_entry {
-      name = "Instrument Box:Waveform - Tune to C4",
+      name = "Instrument Box:Waveform Tuner - Tune to C4",
     invoke = function()
        WaveformTuner(renoise.song().selected_instrument)
     end
 }
 
 renoise.tool():add_keybinding {
-  name = "Global:Waveform Tuner:Waveform - Tune to C4",
+  name = "Global:Waveform Tuner:Waveform Tuner - Tune to C4",
   invoke = function(repeated)
     if (not repeated) then -- we ignore soft repeated keys here
         WaveformTuner(renoise.song().selected_instrument)
